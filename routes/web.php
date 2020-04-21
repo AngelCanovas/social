da95483ce97a85f66d95a2eb515e809aaa28a5f9
Angel <angel.c.m@regmurcia.com>
2020-04-21 20:34:09 +0000

    Fin video 7

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
 <?php
 
-Route::post('statuses', 'StatusesController@store')->name('statuses.store');
+Route::post('statuses', 'StatusesController@store')->name('statuses.store')->middleware('auth');
+
+Route::auth();
--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -12,19 +12,27 @@ class CreateStatusTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
+
+    function guests_users_can_not_create_statuses()
+    {
+        $response = $this->post(route('statuses.store'),['body' => 'Mi primer status']);
+
+        $response->assertRedirect('login');
+    }
+
     public function an_authenticated_user_can_create_statuses()
     {
         $this->withoutExceptionHandling();
 
-        // 1. Given => Teniendo un usuario autenticado
-        // Creamos datos de prueba usando la factoria UserFactory que usa la libreria Faker
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
-        // 2. When => Cuando hace un post request status. Url de la ruta + informacion a enviar
-        $this->post(route('statuses.store'),['body' => 'Mi primer status']);
+        $response = $this->post(route('statuses.store'),['body' => 'Mi primer status']);
 
-        // 3. Then => Entonces veo un nuevo estado en la base de datos
+        $response->assertJson([
+            'body' => 'Mi primer status'
+        ]);
+
         $this->assertDatabaseHas('statuses',[
             'user_id' => $user->id,
             'body' => 'Mi primer status'
