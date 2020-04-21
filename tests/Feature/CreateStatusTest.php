3d3960f51609485f1cd6fc40b16e455b846e10d4
Angel <angel.c.m@regmurcia.com>
2020-04-21 19:09:21 +0000

    Fin video 6

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -9,20 +9,25 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateStatusTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
-    public function a_user_can_create_statuses()
+    public function an_authenticated_user_can_create_statuses()
     {
+        $this->withoutExceptionHandling();
+
         // 1. Given => Teniendo un usuario autenticado
         // Creamos datos de prueba usando la factoria UserFactory que usa la libreria Faker
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
         // 2. When => Cuando hace un post request status. Url de la ruta + informacion a enviar
-        $this->post(route('status.store'),['body' => 'Mi primer status']);
+        $this->post(route('statuses.store'),['body' => 'Mi primer status']);
 
         // 3. Then => Entonces veo un nuevo estado en la base de datos
         $this->assertDatabaseHas('statuses',[
-           'body' => 'Mi primer status'
+            'user_id' => $user->id,
+            'body' => 'Mi primer status'
         ]);
 
     }
