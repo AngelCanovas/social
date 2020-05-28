d5842efca984ce260d660b094ca8d38e4ff55ff3
vagrant <vagrant@homestead>
2020-05-28 11:55:42 +0000

    Fin video 54 Aceptar solicitud con vuejs

--- tests/Browser/UsersCanRequestFriendshipTest.php
+++ tests/Browser/UsersCanRequestFriendshipTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Models\Friendship;
 use App\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
@@ -31,6 +32,33 @@ class UsersCanRequestFriendshipTest extends DuskTestCase
                     ->press('@request-friendship')
                     ->waitForText('Solicitar amistad')
                     ->assertSee('Solicitar amistad')
+            ;
+        });
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function recipients_can_accept_and_deny_friendship_requests()
+    {
+        $sender = factory(User::class)->create();
+        $recipient = factory(User::class)->create();
+
+        Friendship::create([
+            'sender_id' => $sender->id,
+            'recipient_id' => $recipient->id
+        ]);
+
+        $this->browse(function (Browser $browser) use ($sender, $recipient) {
+            $browser->loginAs($recipient)
+                ->visit(route('accept-friendships.index'))
+                ->assertSee($sender->name)
+                ->press('@accept-friendship')
+                ->waitForText('son amigos')
+                ->assertSee('son amigos')
+                ->visit(route('accept-friendships.index'))
+                ->assertSee('son amigos')
             ;
         });
     }
