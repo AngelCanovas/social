1edc679bcca9ebe9f7cfbc6a29d928024132b0e8
vagrant <vagrant@homestead>
2020-06-03 17:43:32 +0000

    Fin video 72 Likes en tiempo real parte 2

--- resources/js/components/StatusListItem.vue
+++ resources/js/components/StatusListItem.vue
@@ -48,7 +48,13 @@
                 required: true
             }
         },
-        components: { LikeBtn, CommentList, CommentForm }
+        components: { LikeBtn, CommentList, CommentForm },
+        mounted() {
+            Echo.channel(`statuses.${this.status.id}.likes`)
+                .listen('ModelLiked', e => {
+                    this.status.likes_count++;
+                });
+        }
     }
 </script>
 
