7f22ea5d58dfa8c04ff001f78bc22f1b5a96ca84
vagrant <vagrant@homestead>
2020-06-04 14:07:09 +0000

    Fin video 75 Como testear Listeners

--- tests/Unit/Traits/HasLikesTest.php
+++ tests/Unit/Traits/HasLikesTest.php
@@ -26,6 +26,8 @@ class HasLikesTest extends TestCase
         Schema::create('model_with_likes', function ($table){
             $table->increments('id');
         });
+
+        Event::fake([ModelLiked::class, ModelUnliked::class]);
     }
 
     /** @test */
@@ -111,7 +113,6 @@ class HasLikesTest extends TestCase
     /** @test */
     public function an_event_is_fired_when_a_model_is_liked()
     {
-        Event::fake([ModelLiked::class]);
         Broadcast::shouldReceive('socket')->andReturn('socket-id');
 
         $this->actingAs(factory(User::class)->create());
@@ -134,7 +135,6 @@ class HasLikesTest extends TestCase
     /** @test */
     public function an_event_is_fired_when_a_model_is_unliked()
     {
-        Event::fake([ModelUnliked::class]);
         Broadcast::shouldReceive('socket')->andReturn('socket-id');
 
         $this->actingAs(factory(User::class)->create());
